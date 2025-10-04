934c06713d886146ff281fb627c81ee603b6215b
наталья конева <koneva77@yandex.ru>
2025-10-05 01:16:59 +0500

    тесты файлов и изменение типа данных аргумента в masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,24 +1,28 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
-
 import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number() -> None:
-    assert get_mask_card_number(1234567890123456) == "1234 56** **** 3456"
+    assert get_mask_card_number('1234567890123456') == "1234 56** **** 3456"
+    assert get_mask_card_number('1234567890987654') == "1234 56** **** 7654"
     with pytest.raises(ValueError) as err:
-        get_mask_card_number(1234567890123456789)
+        get_mask_card_number('1234567890')
         assert str(err.value) == "Проверьте правильность ввода номера карты. Он должен содержать 16 цифр!"
     with pytest.raises(ValueError) as err:
-        get_mask_card_number(1234567890)
+        get_mask_card_number('123456789012345678')
         assert str(err.value) == "Проверьте правильность ввода номера карты. Он должен содержать 16 цифр!"
+        with pytest.raises(ValueError) as err:
+            get_mask_card_number('')
+            assert str(err.value) == "Проверьте правильность ввода номера карты. Он должен содержать 16 цифр!"
 
 
 def test_get_mask_account() -> None:
-    assert get_mask_account(12345678901234567890) == "**7890"
+    assert get_mask_account('12345678901234567890') == "**7890"
+    assert get_mask_account('15975385265435795145') == "**5145"
     with pytest.raises(ValueError) as err:
-        get_mask_account(1234567890123456789078)
+        get_mask_account('1234567890')
         assert str(err.value) == "Проверьте правильность ввода номера счета. Он должен содержать 20 цифр!"
     with pytest.raises(ValueError) as err:
-        get_mask_account(1234567890123456)
+        get_mask_account('1234567890123456789078')
         assert str(err.value) == "Проверьте правильность ввода номера счета. Он должен содержать 20 цифр!"
---
+++ tests/test_processing.py
@@ -0,0 +1,3 @@
+
+
+#def test_filter_by_state()
